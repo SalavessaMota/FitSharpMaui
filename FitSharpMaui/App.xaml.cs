68330627600d2ba9410cd96a467527d91619822f
SalavessaMota <nunosalavessa@hotmail.com>
2024-11-18 15:23:22 +0000

    Add project files.

---
+++ FitSharpMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FitSharpMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FitSharpMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FitSharpMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
